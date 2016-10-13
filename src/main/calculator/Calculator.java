d3626814f4b230e74d3b34e3e302ba226cb36bf5
Orri Arnarsson <orriarnars@gmail.com>
2016-10-13 12:42:55 +0000

    Final commit, commented code blocks and minor whitespace fixes

--- src/main/calculator/Calculator.java
+++ src/main/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 public class Calculator {
+  //The main function of the calculator
   public static int add(String text){
     if(text.equals(""))
       return 0;
@@ -17,14 +18,17 @@ public class Calculator {
     return toInt(text);
   }
 
+  //Parses the string into integers
   public static int toInt(String text) {
     return Integer.parseInt(text);
   }
 
+  //Seperates each integer in the string
   private static String[] split(String text){
     return text.split(",");
   }
 
+  //Adds up the value of each number in the string and returns the total integer value
   public static int total(String[] text) {
     int total = 0;
     for(String number : text) {
@@ -37,14 +41,17 @@ public class Calculator {
     return total;
   }
 
+  //Checks if the string contains \n
   public static boolean hasNewLine(String text) {
     return text.contains("\n");
   }
 
+  //Swaps out the \n character for a comma
   public static String newLineFix(String text) {
     return text.replaceAll("\n", ",");
   }
 
+  //Handles negative number error message output
   private static String negativeNumber(String[] numbers) {
     String error = "Negatives not allowed: ";
     for(String number : numbers) {
@@ -55,14 +62,17 @@ public class Calculator {
     return error;
   }
 
+  //Checks if the string has some special delimeters
   private static boolean hasNewDelimeter(String text) {
     return (text.contains("//") && text.contains("\n"));
   }
 
+  //Swaps out the strange delimeter for a comma, returns a clean, comma seperate string
   private static String fixDelimeter(String text) {
     text = text.substring(2, text.length());
     char delimeter = text.charAt(0);
     text = text.substring(2, text.length());
+
     return text.replaceAll(String.valueOf(delimeter), ",");
   }
 }
